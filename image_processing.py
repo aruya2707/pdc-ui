fa062460bca1e2804535ebed290eb880e7a9eb9b
famolah1717 <famolah17@gmail.com>
2024-04-30 01:24:59 +0700

    integrasi morfologi & count object

--- image_processing.py
+++ image_processing.py
@@ -311,3 +311,77 @@ def threshold(lower_thres, upper_thres):
     img_cpy[condition] = 255
     new_img = Image.fromarray(img_cpy)
     new_img.save("static/img/img_now.jpg")
+
+def dilasi():
+    img = cv2.imread('static/img/img_now.jpg')
+    kernel = np.ones((3,3), np.uint8)
+    dilated_img = cv2.dilate(img, kernel, iterations=1)
+    new_img = Image.fromarray(dilated_img)
+    new_img.save('static/img/img_now.jpg')
+
+def erosi():
+    img = cv2.imread('static/img/img_now.jpg')
+    kernel = np.ones((3,3), np.uint8)
+    erode_img = cv2.erode(img, kernel, iterations=1)
+    new_img = Image.fromarray(erode_img)
+    new_img.save('static/img/img_now.jpg')
+
+def opening():
+    img = cv2.imread('static/img/img_now.jpg')
+    kernel = np.ones((3,3), np.uint8)
+    erode_img = cv2.erode(img, kernel, iterations=1)
+    dilated_img = cv2.dilate(erode_img, kernel, iterations=1)
+    new_img = Image.fromarray(dilated_img)
+    new_img.save('static/img/img_now.jpg')
+
+def closing():
+    img = cv2.imread('static/img/img_now.jpg')
+    kernel = np.ones((3,3), np.uint8)
+    dilated_img = cv2.dilate(img, kernel, iterations=1)
+    erode_img = cv2.erode(dilated_img, kernel, iterations=1)
+    new_img = Image.fromarray(erode_img)
+    new_img.save('static/img/img_now.jpg')
+
+def count_object1():
+    img = cv2.imread('static/img/img_now.jpg', 0)
+
+    # noise removal
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=1)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.4 * dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers + 1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+
+    # Watershed
+    img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    markers = cv2.watershed(img_color, markers)
+    img_color[markers == -1] = [255, 0, 0]
+
+    # Calculate selected count
+    # Exclude the background (label 1) and regions with size equal to the image size
+    shape_count = len(np.unique(markers)) - 2  # Subtract 2 for background and image size
+
+    new_img = Image.fromarray(img_color)
+    new_img.save('static/img/img_now.jpg')
+    
+    message = "jumlah objek : " + str(shape_count)
+    return message

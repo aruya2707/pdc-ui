e74ca12351269980b5424f2f199f59504356adf8
lindasantika08 <linda.santika.tif22@polban.ac.id>
2024-04-29 22:01:38 +0700

    commit buat hitung citra (yang 2) kaca belooommm

--- image_processing.py
+++ image_processing.py
@@ -29,7 +29,7 @@ def is_grey_scale(img_path):
                 return False
     return True
 
-# tidak ada perubahan unutk zoom karna ada batasan
+# tidak ada perubahan untuk zoom karna ada batasan
 def zoomin():
     img = Image.open("static/img/img_now.jpg")
     img = img.convert("RGB")
@@ -311,3 +311,57 @@ def threshold(lower_thres, upper_thres):
     img_cpy[condition] = 255
     new_img = Image.fromarray(img_cpy)
     new_img.save("static/img/img_now.jpg")
+
+
+# Membaca dan menghitung citra
+def count_selected_shapes(image_path):
+    img = cv2.imread(image_path, 0)
+
+    # noise removal
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=1)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.4 * dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers + 1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+
+    # Watershed
+    img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    markers = cv2.watershed(img_color, markers)
+    img_color[markers == -1] = [255, 0, 0]
+
+    # Calculate selected count
+    # Exclude the background (label 1) and regions with size equal to the image size
+    shape_count = len(np.unique(markers)) - 2  # Subtract 2 for background and image size
+
+    return shape_count, img_color
+
+
+def plot_image(image, figsize=(18, 18)):
+    fig, ax = plt.subplots(figsize=(18, 20))
+    ax.imshow(image, cmap='gray')
+    plt.show()
+
+
+image_path = 'hitung2.jpg'
+shape_count, result_image = count_selected_shapes(image_path)
+print("Selected shape Count:", shape_count)
+plot_image(result_image)
